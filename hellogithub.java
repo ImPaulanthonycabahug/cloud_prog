e2aede933e384150a59816809366ba115db60f50
ImPaulanthonycabahug <150148232+ImPaulanthonycabahug@users.noreply.github.com>
2025-02-10 15:12:40 +0800

    Update hellogithub.java

--- hellogithub.java
+++ hellogithub.java
@@ -1,5 +1,8 @@
+import java.time.LocalDateTime;
+
 public class hellogithub {
     public static void main(String[] args) {
         System.out.println("Hello GIthub!!");
+        System.out.println("Current Date and Time: " + LocalDateTime.now());
     }
 }
